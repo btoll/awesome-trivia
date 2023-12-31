35d3132f94d4953774ccd5390735c5f05326ef2b
Benjamin Toll <ben@benjamintoll.com>
2023-12-30 20:33:17 -0500

    Simplifying logic

--- go.mod
+++ go.mod
@@ -2,6 +2,7 @@ module github.com/btoll/awesome-trivia
 
 go 1.19
 
-require golang.org/x/net v0.7.0
-
-require github.com/btoll/trivial v0.2.0
+require (
+	github.com/btoll/trivial v0.2.1 // indirect
+	golang.org/x/net v0.19.0 // indirect
+)
--- main.go
+++ main.go
@@ -2,45 +2,73 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"log"
 	"net/url"
 	"strconv"
+	"strings"
 
-	"github.com/btoll/trivial"
-	"golang.org/x/net/websocket"
+	"github.com/btoll/awesome-trivia/trivial"
 )
 
 var (
-	serverURL       = flag.String("u", "wss://192.168.1.96:3000", "URL of game websocket server")
-	gameName        = flag.String("n", "default", "Name of game")
-	tokenExpiration = flag.Float64("e", 3600, "Token expiration (in seconds)")
+	wssURL          = flag.String("wss", "wss://127.0.0.1:3000", "URL of game websocket server")
+	hostURL         = flag.String("host", "https://127.0.0.1:3000", "URL of game host server")
+	gameName        = flag.String("game", "default", "Name of game")
+	generateCert    = flag.Bool("generateCert", false, "Generate a new TLS certificate")
+	tokenExpiration = flag.Float64("tokenExpiration", 3600, "Token expiration (in seconds)")
 )
 
-func main() {
-	flag.Parse()
-
-	parsedUrl, err := url.Parse(*serverURL)
+func parseURL(s string) trivial.Socket {
+	parsedUrl, err := url.Parse(s)
 	if err != nil {
-		log.Fatalln("game server uri could not be parsed")
+		log.Fatalln("server url could not be parsed")
 	}
 	port, err := strconv.Atoi(parsedUrl.Port())
 	if err != nil {
 		log.Fatalln("port could not be parsed")
 	}
-	server := trivial.NewSocketServer(trivial.URL{
-		Sock: trivial.Socket{
-			Protocol: parsedUrl.Scheme,
-			Domain:   parsedUrl.Hostname(),
-			Port:     port,
-		},
+	return trivial.Socket{
+		Protocol: parsedUrl.Scheme,
+		Domain:   parsedUrl.Hostname(),
+		Port:     port,
+	}
+}
+
+func bound(n int) string {
+	return strings.Repeat("-", n)
+}
+
+func main() {
+	flag.Parse()
+
+	wssSock := parseURL(*wssURL)
+	hostSock := parseURL(*hostURL)
+	socketServer := trivial.URL{
+		Sock: wssSock,
 		Path: "ws",
-	}, trivial.TLSCert{
-		EcdsaCurve: "P384",
-		Host:       "127.0.0.1,192.168.1.96",
-		IsCA:       true,
-		RsaBits:    3072,
-	})
+	}
+
+	server := trivial.NewSocketServer(socketServer)
+	fmt.Printf("%s\ncreated new websocket server `%s`\n",
+		bound(75),
+		socketServer)
+
+	if *generateCert {
+		trivial.GenerateCert(trivial.TLSCert{
+			EcdsaCurve: "P384",
+			Host:       fmt.Sprintf("%s,%s", "127.0.0.1", hostSock.Domain),
+			IsCA:       true,
+			RsaBits:    3072,
+		})
+		fmt.Printf("generated new TLS certificate for domains `%s` and `%s`\n", "127.0.0.1", hostSock.Domain)
+	}
 
 	game := trivial.NewGame(*gameName, *tokenExpiration)
+	fmt.Printf("registered game `%s` with key `%s` on host `%s`\n%s\n",
+		game.Name,
+		game.Key.Key,
+		hostSock,
+		bound(75))
 	server.RegisterAndStartGame(game)
 }
